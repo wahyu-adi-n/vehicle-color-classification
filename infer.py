8909ba50ae900e5233a1f6acbd8f75657989588a
Wahyu Adi Nugroho <wahyuadinugrohoasinan@gmail.com>
2022-11-18 09:13:53 +0700

    add running time inference

--- infer.py
+++ infer.py
@@ -1,10 +1,10 @@
-import torch
 from PIL import Image
 from torchvision import transforms as T
 from model.vcm_cnn import VehicleColorModel
 from config import *
 from utils.lib import *
-
+import torch
+import time
 
 model = VehicleColorModel()
 model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device("cpu")))
@@ -19,11 +19,14 @@ transforms = T.Compose([
 def infer(image):
     image = transforms(image)
     image = image.unsqueeze(0)
+    t0 = time.time()
     pred = model.forward(image).argmax(dim=1)
+    runtime = time.time() - t0
     class_label = decode_label(pred)
-    return class_label
+    return class_label, runtime
 
 
 if __name__ == "__main__":
     image = Image.open(IMAGE_PATH).convert('RGB')
-    print(infer(image))
+    label, time = infer(image)
+    print(f"This image is predicted to {label}. Running on {time/1000:.4f}s")
